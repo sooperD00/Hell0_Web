0eabe228290a93b08c5516a6d66093423dc8a120
sooperD00 <nicole.rowsey@gmail.com>
2025-09-26 15:42:08 -0700

    feat(core): implement AsciiInputModel, no validation (RED)

---
+++ Hell0_Web/Models/AsciiInputModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Hell0_Web.Models
+{
+    public class AsciiInputModel
+    {
+        public string UserInput { get; set; }
+
+        // Optional: font selection for future stretch goal
+        public string Font { get; set; } = "Standard";
+    }
+}
