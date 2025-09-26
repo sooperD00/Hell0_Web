3b72f9f1d9987c633c53d1af1e2246cd965b5c27
sooperD00 <nicole.rowsey@gmail.com>
2025-09-26 16:47:27 -0700

    test(controller): add failing AsciiController tests (GET and POST actions)

---
+++ Hell0_Web.Tests/AsciiControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Hell0_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hell0_Web.Tests
+{
+    internal class AsciiControllerTests
+    {
+        [Fact]
+        public void Input_Get_ReturnsViewWithAsciiInputModel()
+        {
+            var controller = new AsciiController();
+
+            var result = controller.Input();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.IsType<AsciiInputModel>(viewResult.Model);
+        }
+
+        [Fact]
+        public void Post_Input_ReturnsAsciiOutput()
+        {
+            var controller = new AsciiController();
+            var model = new AsciiInputModel { UserInput = "Test" };
+
+            var result = controller.Input(model);
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var output = viewResult.ViewData["AsciiOutput"] as string;
+            Assert.Contains("T", output); // basic sanity check
+        }
+    }
+}
